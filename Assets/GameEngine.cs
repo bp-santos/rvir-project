437ae364917e93865270628375f483b41337617e
bernardo <62389716+bp-santos@users.noreply.github.com>
2023-12-16 14:41:17 +0000

    lab-assignment-2

--- Assets/GameEngine.cs
+++ Assets/GameEngine.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using System.IO;
 using TMPro;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(InputData))]
 public class GameEngine : MonoBehaviour
@@ -19,12 +21,14 @@ public class GameEngine : MonoBehaviour
     public Material[] materials;
     public Material selectedMaterial;
     public TextMeshPro timeText;
+    public int selectionTechnique;
 
     private void Start()
     {
         _inputData = GetComponent<InputData>();
         clickCount = 0;
         isRunning = false;
+        SetRays();
         UpdateTimeText();
     }
 
@@ -77,17 +81,125 @@ public class GameEngine : MonoBehaviour
                 {
                     leftGripPressed = false;
                 }
+
+                if (rightButtonState && leftButtonState)
+                {   
+                    GameObject obj1 = null;
+                    GameObject obj2 = null;
+
+                    if(selectionTechnique == 1)
+                    {
+                        obj1 = GetObject(_inputData._leftController,"_image");
+                        obj2 = GetObject(_inputData._rightController,"_frame");
+                    }
+
+                    if(selectionTechnique == 2){
+                        obj1 = GetObject(_inputData._cameraRay, "_image");
+                        obj2 = GetObject(_inputData._rightController, "_frame");
+                    }
+
+                    if(obj1 != null && obj2 != null && obj1 != obj2){
+                        Bounds bounds1 = obj1.GetComponent<Renderer>().bounds;
+                        Bounds bounds2 = obj2.GetComponent<Renderer>().bounds;
+
+                        if (Vector3.Distance(bounds1.size, bounds2.size) < 0.000001)
+                        {
+                            obj1.GetComponent<Renderer>().material = selectedMaterial;
+                            obj2.GetComponent<Renderer>().material = selectedMaterial;
+                        }
+                    }
+                }
             }
 
             if (CheckTargetMaterials())
             {
                 Debug.Log("Finished");
                 isRunning = false;
-                Debug.Log(elapsedTime);
                 SaveTimeToCSV();
             }
         }
     }
+
+    private void SetRays()
+    {
+        switch (selectionTechnique)
+        {
+            case 0:
+                SetInteractorActive(false, "_leftController");
+                SetInteractorActive(true, "_rightController");
+                SetInteractorActive(false, "_ray");
+                break;
+            case 1:
+                SetInteractorActive(true, "_leftController");
+                SetInteractorActive(true, "_rightController");
+                SetInteractorActive(false, "_ray");
+                break;
+            case 2:
+                SetInteractorActive(false, "_leftController");
+                SetInteractorActive(true, "_rightController");
+                SetInteractorActive(true, "_ray");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetInteractorActive(bool isActive, string tag)
+    {
+        foreach (var obj in GameObject.FindGameObjectsWithTag(tag))
+        {
+            XRBaseInteractor interactor = obj.GetComponent<XRBaseInteractor>();
+            if (interactor != null)
+            {
+                interactor.enabled = isActive;
+            }
+        }
+    }
+
+    private GameObject GetObject(InputDevice controller, string tag)
+    {
+        if (controller.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 controllerPosition) &&
+            controller.TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion controllerRotation))
+        {
+
+            RaycastHit hit;
+            Ray ray = new Ray(controllerPosition, controllerRotation * Vector3.forward);
+
+            //if(selectionTechnique == 1){
+            //    ray = new Ray(controllerPosition, controllerRotation * Vector3.forward);
+            //}
+            if(selectionTechnique == 2){
+                Vector3 adjustedPosition = new Vector3(controllerPosition.x, controllerPosition.y + 0.2f, controllerPosition.z);
+                ray = new Ray(adjustedPosition, controllerRotation * Vector3.forward);
+            }
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject obj = hit.collider.gameObject;
+                if(obj.CompareTag(tag))
+                {
+                    return obj;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public string GetInteractionTechniqueName()
+    {
+        switch(selectionTechnique){
+            case 0:
+                return "One hand";
+            case 1:
+                return "Two hands";
+            case 2:
+                return "DH and HMD";
+            default:
+                return "NaN";
+        }
+    }
+
 
     private void restartMaterials()
     {
@@ -116,10 +228,10 @@ public class GameEngine : MonoBehaviour
 
         if (!File.Exists(filePath))
         {
-            File.WriteAllText(filePath, "Clicks,Errors,Minutes,Seconds,Milliseconds\n");
+            File.WriteAllText(filePath, "Technique,Clicks,Errors,Minutes,Seconds,Milliseconds\n");
         }
 
-        string timeData = $"{clickCount},{clickCount-8},{Mathf.FloorToInt(elapsedTime / 60f)},{Mathf.FloorToInt(elapsedTime % 60f)},{Mathf.FloorToInt((elapsedTime * 1000) % 1000)}\n";
+        string timeData = $"{GetInteractionTechniqueName()},{clickCount},{clickCount-8},{Mathf.FloorToInt(elapsedTime / 60f)},{Mathf.FloorToInt(elapsedTime % 60f)},{Mathf.FloorToInt((elapsedTime * 1000) % 1000)}\n";
         File.AppendAllText(filePath, timeData);
     }
 
--- Assets/InputData.cs
+++ Assets/InputData.cs
@@ -5,34 +5,34 @@ using UnityEngine.XR;
 
 public class InputData : MonoBehaviour
 {
-	public InputDevice _rightController;
-	public InputDevice _leftController;
-	public InputDevice _HMD;
+    public InputDevice _rightController;
+    public InputDevice _leftController;
+    public InputDevice _cameraRay; // Updated variable name
 
-	void Update()
-	{
-		if (!_rightController.isValid || !_leftController.isValid || !_HMD.isValid)
-			InitializeInputDevice();
-	}
+    void Update()
+    {
+        if (!_rightController.isValid || !_leftController.isValid || !_cameraRay.isValid)
+            InitializeInputDevice();
+    }
 
-	private void InitializeInputDevice()
-	{
-		if (!_rightController.isValid)
-			InitializeInputDevice(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right, ref _rightController);
-		if (!_leftController.isValid)
-			InitializeInputDevice(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left, ref _leftController);
-		if (!_HMD.isValid)
-			InitializeInputDevice(InputDeviceCharacteristics.HeadMounted, ref _HMD);  
-	}
+    private void InitializeInputDevice()
+    {
+        if (!_rightController.isValid)
+            InitializeInputDevice(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Right, ref _rightController);
+        if (!_leftController.isValid)
+            InitializeInputDevice(InputDeviceCharacteristics.Controller | InputDeviceCharacteristics.Left, ref _leftController);
+        if (!_cameraRay.isValid)
+            InitializeInputDevice(InputDeviceCharacteristics.HeadMounted, ref _cameraRay); // Updated usage
+    }
 
-	private void InitializeInputDevice(InputDeviceCharacteristics inputCharacteristics, ref InputDevice inputDevice)
-	{
-		List<InputDevice> devices = new List<InputDevice>();
-		InputDevices.GetDevicesWithCharacteristics(inputCharacteristics, devices);
+    private void InitializeInputDevice(InputDeviceCharacteristics inputCharacteristics, ref InputDevice inputDevice)
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(inputCharacteristics, devices);
 
-		if (devices.Count > 0) 
-		{
-			inputDevice = devices[0];
-		}
-	}
+        if (devices.Count > 0)
+        {
+            inputDevice = devices[0];
+        }
+    }
 }
